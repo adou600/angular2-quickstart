3f9a826081833d526a47790e151e56699f4bcbce
adou600 <adrien.nicolet@gmail.com>
2016-10-11 17:00:08 +0200

    add routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,11 +6,16 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
 
     getHeroesSlowly(): Promise<Hero[]> {
-        return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000))
+        return new Promise<Hero[]>(resolve => setTimeout(resolve, 500))
             .then(() => this.getHeroes());
     }
 
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
     }
 }
